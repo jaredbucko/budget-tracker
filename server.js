8d67ba16a19b87ff1b974a22e80096c13d413e45
jaredbucko <jaredbucko@icloud.com>
2020-03-08 13:02:17 -0700

    update initial commit

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+  useFindAndModify: false
 });
 
-// routes here
+// routes
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
